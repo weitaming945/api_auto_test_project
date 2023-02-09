0fd57552f794c292842be597a60c2f0b4d9ea38d
刘玲 <liu.ling@yuemia.com>
2023-02-09 11:14:55 +0800

    添加了消息发送到企业微信相关代码

--- run_liuling.py
+++ run_liuling.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # @Time:2021/6/21 16:11
 # @Author:lucky
@@ -9,7 +10,10 @@ import time
 import os
 import pytest
 from common.handle_log import log
-
+from pytest_jsonreport.plugin import JSONReport
+from common.handle_robot import robot
+from common.handle_jenkins import jenk
+from common.handle_config import conf
 
 # report_dir=os.path.join(REPORTS_DIR,filename)
 
@@ -20,4 +24,24 @@ os.system('cd reports && del * /q && cd ..')
 # log.info("清空日志文件")
 # os.system('cd ./logs && del * /q')
 
-pytest.main(['testcases/huarun999/test_huarun999_middle.py','-s','-v','--alluredir=reports'])
+#需要生成结果文件，不然jenkins识别不出来
+plugin=JSONReport()
+
+pytest.main(['testcases/huarun999/test_huarun999_middle.py','-s','-v','--alluredir=reports'],plugins=[plugin])
+
+summary=plugin.report.get("summary")
+#得到通过，失败，跳过和总执行的用例数
+passed=summary.get("passed",0)
+failed=summary.get("failed",0)
+skipped=summary.get("skipped",0)
+total=summary.get("total",0)
+
+
+#获取地址
+#获取项目名称
+projectname=conf.get("jenkins","project_name")
+address=jenk.get_allure_address(projectname=projectname,key="nextBuildNumber")
+print(address)
+
+#发送企业微信消息
+robot.sendMessage(projectname=projectname,total=total,passed=passed,skipped=skipped,failed=failed,address=address)
